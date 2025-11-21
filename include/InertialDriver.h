70bfa80463d8ab5d26a7a0cb94d300f59f61372f
huberemanuelprog <emanuel.huber@studenti.unipd.it>
2025-11-21 17:06:07 +0100

    Update InertialDriver.h

--- include/InertialDriver.h
+++ include/InertialDriver.h
@@ -1,19 +1,21 @@
 #ifndef INERTIALDRIVER_H
 #define INERTIALDRIVER_H
 
+
 class InertialDriver{
 
   public : 
-  
-    // Reading: std::array<double, 6>
+
+    InertialDriver();
+    // (Letture) = SensorData: std::array<double, 6>
     // Measure : std::array<std::array<double, 6>, 17>
     
-    void push_back(const std::array<std::array<double, 6>, 17>& measure);
-    std::array<std::array<double, 6>, 17> pop_front();
+    void push_back(const Measure& measure);
+    Measure pop_front();
     void clear_buffer();
-    std::array<double, 6> get_reading(int index);
+    SensorData get_reading(int index);
     
-    std::ostream& operator<<(std::ostream& os, const InertialDriver& v);
+    friend std::ostream& operator<<(std::ostream& os, const InertialDriver& v);
 
   private:
     MyVector<Measure> buffer_;
