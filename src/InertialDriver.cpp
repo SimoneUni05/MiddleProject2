cdcfbf62528db39adc62d9e000dfa3d19546450c
Manuel Carelli <145465913+Broccolo02@users.noreply.github.com>
2025-11-22 12:20:41 +0100

    Implement constructor and operator<< for InertialDriver
    Added constructor implementation and operator<< overload for InertialDriver.

--- src/InertialDriver.cpp
+++ src/InertialDriver.cpp
@@ -1,2 +1,15 @@
 #include "../include/InertialDriver.h"
+//Implementazione costruttore
+InertialDriver::InertialDriver(): buffer_ {MyVector<Measure>()}, head_ {0}, tail_ {0}, size_{0} {};
+
+// Implementazione dell'operator <<
+std::ostream& operator<<(std::ostream& os, const InertialDriver& v) {
+	  for (int i=v.head_;i>v.tail_;i--){ //scorrimento del buffer con le misure dalla più recente alla più vecchia
+		    os << v.head_-i + 1 << " measure:" << buffer[i] << '\n';
+	  }
+	  os <<'\n';
+  }
+  return os;
+};
+
 
