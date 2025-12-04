f723cb4f28504afe0617d5e20c4f241398197da8
Simone Piccinini <AD\piccsim51647@DEI-LPCB2D-017.vdi.ict.unipd.it>
2025-12-04 13:47:36 +0100

    added cMake

---
+++ build/CMakeFiles/libInertialDriver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libInertialDriver.dir/src/InertialDriver.cpp.o"
+  "CMakeFiles/libInertialDriver.dir/src/InertialDriver.cpp.o.d"
+  "liblibInertialDriver.pdb"
+  "liblibInertialDriver.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libInertialDriver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
